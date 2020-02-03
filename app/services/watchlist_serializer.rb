f830520aabd6220703563a74349a4bb4c6fa266b
flow1981 <contact.f.stagliano@gmail.com>
2020-02-04 07:58:00 +0800

    isolated serializer deployment error

--- app/services/watchlist_serializer.rb
+++ app/services/watchlist_serializer.rb
@@ -7,10 +7,10 @@ class WatchlistSerializer
     def to_serialized_json
         @watchlist_object.to_json(
             :only => [:name, :id],
-            :include => {   :user => { :only => [:username]}}
-        )
-                            # :satellites => { :only => [:id, :name, :line1,:line2, :constellation_id, :displayed]}
-            
+            :include => {   :user => { :only => [:username]}
+                            :satellites => { :only => [:id, :name]}
+            }
+    )
         
     end
 
