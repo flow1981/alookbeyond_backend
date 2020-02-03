9c504bac722d5885dc083faad23bae3d9ea1ed14
flow1981 <contact.f.stagliano@gmail.com>
2020-02-04 07:45:52 +0800

    trouble shooting watchlist serializer for deployment

--- app/services/watchlist_serializer.rb
+++ app/services/watchlist_serializer.rb
@@ -6,9 +6,9 @@ class WatchlistSerializer
     
     def to_serialized_json
         @watchlist_object.to_json(
-        :only => [:name, :id])
-        # ,
-        # :include => {:user => { :only => [:username]},
+        :only => [:name, :id]),
+        :include => {:user => { :only => [:username]}
+        ,
         #             :satellites => { :only => [:id, :name, :line1,:line2, :constellation_id, :displayed]},
         #             }
         
